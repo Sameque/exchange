8bc0b835e1b54205680d247b470defb582c2a84c
Sameque Sant'ana <samequesantana@gmail.com>
2026-07-07 10:23:40 -0300

    refactor(ordergenerator): load db connection from configuration

--- src/OrderGenerator/OrderGenerator.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/OrderGenerator/OrderGenerator.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OrderGenerator.Domain.Interfaces;
 using OrderGenerator.Infrastructure.Configuration;
@@ -10,13 +11,17 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection services,
+        IConfiguration configuration,
         Action<ExchangeSettings>? configureExchangeSettings = null)
     {
         if (configureExchangeSettings is not null)
             services.Configure(configureExchangeSettings);
 
+        var connectionString = configuration.GetConnectionString("DefaultConnection")
+            ?? "Host=localhost;Port=5432;Database=orderdb;Username=postgres;Password=postgres";
+
         services.AddDbContext<OrderDbContext>(options =>
-            options.UseSqlite("Data Source=orders.db"));
+            options.UseNpgsql(connectionString));
 
         services.AddScoped<IOrderRepository, OrderRepository>();
 
